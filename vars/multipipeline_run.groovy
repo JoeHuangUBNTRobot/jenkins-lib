47155d900f3cbb9d05cf167adeabd8a02786f987
Ken Lu <ken.lu@ui.com>
2020-09-24 15:44:12 +0800

    Ignore build when build_jobs.size() is 0

--- vars/multipipeline_run.groovy
+++ vars/multipipeline_run.groovy
@@ -1,8 +1,8 @@
 def call(String project, String build_target, Map build_series=[:], Map job_options=[:]) {
-	
+
 	timestamps {
 		def parallel_jobs = [:]
-		
+
 		// fill job options here
 		job_options << ubnt_builders.get_job_options(project)
 
@@ -10,7 +10,11 @@ def call(String project, String build_target, Map build_series=[:], Map job_opti
 		println "job_options: $job_options"
 
 		def build_jobs = ubnt_builders."${project}"(build_target, job_options, build_series)
-		
+
+		if (build_jobs.size() == 0) {
+			return
+		}
+
 		for (build_job in build_jobs) {
 			parallel_jobs[build_job.name] = ubnt_general_build(build_job)
 		}
@@ -37,12 +41,12 @@ def call(String project, String build_target, Map build_series=[:], Map job_opti
 		} catch (Exception e) {
 			throw e
 		} finally {
-			// TODO: notification or something else 
+			// TODO: notification or something else
 			def job_names = parallel_jobs.keySet().sort()
 			def mail_body=''
 			def project_build_status='Success'
 			def tag_build=false
-			job_names.each{ k-> 
+			job_names.each{ k->
 				def build_job = parallel_jobs[k].build_job
 				if (build_job.build_status == false) {
 					project_build_status = 'Failed'
