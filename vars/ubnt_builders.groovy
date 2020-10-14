62c8f239e176e792fb1169f3462b7bf9854213f6
Steve Chen <steve.chen@ui.com>
2020-10-14 17:37:51 +0800

    fix debbox use latest sublink

--- vars/ubnt_builders.groovy
+++ vars/ubnt_builders.groovy
@@ -489,7 +489,7 @@ def debbox_builder(String productSeries, Map job_options=[:], Map build_series=[
 					if (m.upload && m.containsKey('upload_info')) {
 						def upload_path = m.upload_info.path.join('/')
 						def latest_path = m.upload_info.latest_path.join('/')
-						m.nasinfo = ubnt_nas.upload(m.docker_artifact_path, upload_path, latest_path, is_tag)
+						m.nasinfo = ubnt_nas.upload(m.docker_artifact_path, upload_path, latest_path, true)
 					}
 				}
 			},
