107c8c30fc484b5bcc7ed4b0c4635f156b5c5585
Steve Chen <steve.chen@ui.com>
2020-10-23 17:29:41 +0800

    add slack_helper

---
+++ vars/slack_helper.groovy
@@ -0,0 +1,8 @@
+def notification(String buildResult)
+{
+	if (buildResult == "SUCCESS") {
+		slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+	} else {
+		slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+	}
+}
