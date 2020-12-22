eb0fdd60ee1dfefd60ba0c6eeac06416bf7b77a0
Steve Chen <steve.chen@ui.com>
2020-12-22 15:11:23 +0800

    fix: use env.env.CHANGE_BRANCH as ref name

--- vars/ubnt_builders.groovy
+++ vars/ubnt_builders.groovy
@@ -436,6 +436,9 @@ def debbox_builder(String productSeries, Map job_options=[:], Map build_series=[
                                 }
                                 println "tag build: istag: $is_tag, is_atag:$is_atag"
                                 git_args.local_branch = ref
+                            } else if (is_pr) {
+                                // use change branch as ref
+                                ref = env.CHANGE_BRANCH
                             } else {
                                 ref = git_helper.current_branch()
                                 if (!ref || ref == 'HEAD') {
