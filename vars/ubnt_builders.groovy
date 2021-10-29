52aeba6163c2eefb3e7d75b2f2ee98a85dd86563
shawnhsiao1 <shawn.hsiao@ui.com>
2021-10-29 10:20:12 +0800

    Add UDR smoke test

--- vars/ubnt_builders.groovy
+++ vars/ubnt_builders.groovy
@@ -631,8 +631,8 @@ def debbox_builder(String productSeries, Map job_options=[:], Map build_series=[
                         }
                     }
 
-                    // skip UDMPSE, UDR and UDW test
-                    if (name == 'UDMPROSE' || name == 'UDR' || name == 'UDW' || name == 'UDMPRO' || name == 'UDWPRO' || name == 'UDMLITE' || name == 'UDK') {
+                    // skip UDMPSE and UDW test
+                    if (name == 'UDMPROSE' || name == 'UDW' || name == 'UDMPRO' || name == 'UDWPRO' || name == 'UDMLITE' || name == 'UDK') {
                     	return
                     }
 
