71f1e2794cd0fc52769fde45f7622b200d6fe200
HankChang-UI <83639960+HankChang-UI@users.noreply.github.com>
2022-03-08 11:35:55 +0800

    analytic report support bullseye (#33)

--- vars/ubnt_builders.groovy
+++ vars/ubnt_builders.groovy
@@ -795,7 +795,7 @@ def disk_quota_builder(String productSeries, Map job_options=[:], Map build_seri
 
 def analytic_report_builder(String productSeries, Map job_options=[:], Map build_series=[:]) {
     echo "build $productSeries"
-    return debpkg(job_options, ['stretch/arm64'])
+    return debpkg(job_options, ['stretch/arm64', 'bullseye/arm64'])
 }
 
 def debbox_base_files_builder(String productSeries, Map job_options=[:], Map build_series=[:]) {
