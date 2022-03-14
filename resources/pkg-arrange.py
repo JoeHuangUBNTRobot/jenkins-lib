71b45f6da652949f6000f3527b795cad26a1ee42
John Liu <john.liu@ubnt.com>
2022-03-14 11:16:07 +0800

    support package makefile with multi-version of different distribution (#28)
    * support package makefile with multi-version of different distribution
    
    * change back version compare function

--- resources/pkg-arrange.py
+++ resources/pkg-arrange.py
@@ -18,21 +18,28 @@ pkg_series_pattern = [
 
 
 class PkgMkInfo:
-    def __init__(self, series, name, version, arch, multi_var_name):
+    def __init__(self, series, name, version, arch, multi_var_name, dist):
         self.name = name
-        self.version = version
         self.arch = arch
-        self.deb_name = ''
         self.series = series
-        self.md5sum_list = dict()
-        self.multi_var_name = multi_var_name
+        self.dist_info = dict()
+        self.add_dist_info(version, multi_var_name, dist)
 
-    def add_md5sum(self, multi_name, deb_file):
-        self.deb_name = deb_file.name
+    def add_dist_info(self, version, multi_var_name, dist):
+        if dist not in self.dist_info:
+            self.dist_info[dist] = {
+                'version': version,
+                'deb_name': '',
+                'md5sum_list': dict(),
+                'multi_var_name': multi_var_name
+            }
+
+    def add_md5sum(self, multi_name, deb_file, dist):
+        self.dist_info[dist]['deb_name'] = deb_file.name
         m = hashlib.md5()
         m.update(deb_file.read_bytes())
 
-        self.md5sum_list[multi_name] = m.hexdigest()
+        self.dist_info[dist]['md5sum_list'][multi_name] = m.hexdigest()
 
     def generate_makefile(self, makefile, base_url):
         variable_name_prefix = self.name.upper().replace('-', '_')
@@ -44,38 +51,46 @@ class PkgMkInfo:
         if self.arch == 'all':
             arch = 'all'
 
-        sorted_md5_list = sorted(self.md5sum_list.items())
+        for dist, info in self.dist_info.items():
+            info['md5sum_list'] = sorted(info['md5sum_list'].items())
 
+        # Only show one download address on top of file
         makefile.write('# {}\n\n'.format('/'.join([
             base_url,
             self.series,
-            sorted_md5_list[0][0],
-            self.deb_name,
+            info['md5sum_list'][0][0],
+            info['deb_name'],
         ])))
+
         makefile.write('{}:={}\n'.format(variable_pkg_name, self.name))
-        makefile.write('{}:={}\n\n'.format(variable_pkg_version, self.version))
+
+        for dist, info in self.dist_info.items():
+            makefile.write('{}_{}_VERSION:={}\n'.format(
+                variable_name_prefix, dist, info['version']))
+            for multi_name, md5sum in info['md5sum_list']:
+                makefile.write('{}_{}_MD5:={}\n'.format(
+                    variable_name_prefix, multi_name.replace('/', '_'),
+                    md5sum))
+
+        makefile.write('\n{}:=$(value {}_$(_distro)_VERSION)\n'.format(
+            variable_pkg_version, variable_name_prefix))
 
         base_url = '/'.join([
             base_url,
             self.series,
             '$(_distro)',
         ])
-
-        if self.multi_var_name:
-            base_url += '/$({})'.format(self.multi_var_name)
-
-        for multi_name, md5sum in sorted_md5_list:
-            makefile.write('{}_{}_MD5:={}\n'.format(
-                variable_name_prefix, multi_name.replace('/', '_'), md5sum))
+        if info['multi_var_name']:
+            base_url += '/$({})'.format(info['multi_var_name'])
 
         makefile.write('{}:={}\n\n'.format(variable_base_url, base_url))
         makefile.write('PKG_FILE:=$({})_$({})_{}.deb\n'.format(
             variable_pkg_name, variable_pkg_version, arch))
 
-        if self.multi_var_name:
+        if info['multi_var_name']:
             makefile.write(
                 'PKG_FILE_MD5SUM:=$(value {}_$(_distro)_$({})_MD5)\n'.format(
-                    variable_name_prefix, self.multi_var_name))
+                    variable_name_prefix, info['multi_var_name']))
         else:
             makefile.write(
                 'PKG_FILE_MD5SUM:=$(value {}_$(_distro)_MD5)\n'.format(
@@ -166,10 +181,10 @@ def get_multi_var_name(pkg_name):
 def deb_pkg_name_parser(name):
     tokens = re.split(r'[._]', name)
     pkg_name = tokens[0]
-    pkg_verion = '.'.join(tokens[1:-1])
+    pkg_version = '.'.join(tokens[1:-1])
     pkg_arch = tokens[-1]
     pkg_series = get_pkg_series(pkg_name)
-    return pkg_name, pkg_verion, pkg_arch, pkg_series
+    return pkg_name, pkg_version, pkg_arch, pkg_series
 
 
 def arrange_directory(args):
@@ -178,11 +193,15 @@ def arrange_directory(args):
         if filter_files_not_handled(f, args):
             continue
 
-        pkg_name, pkg_verion, pkg_arch, pkg_series = deb_pkg_name_parser(
+        pkg_name, pkg_version, pkg_arch, pkg_series = deb_pkg_name_parser(
             str(f.stem))
 
         relpath = f.relative_to(args.directory)
         multi_name = str(relpath.parent)
+        if '/' in multi_name:
+            dist = multi_name.split('/', 1)[0]
+        else:
+            dist = multi_name
         multi_var_name = get_multi_var_name(pkg_series)
 
         dst_path = args.directory / pkg_series / relpath
@@ -190,9 +209,13 @@ def arrange_directory(args):
         if f.suffix == '.deb':
             if pkg_name not in pkg_info_list:
                 pkg_info_list[pkg_name] = PkgMkInfo(pkg_series, pkg_name,
-                                                    pkg_verion, pkg_arch,
-                                                    multi_var_name)
-            pkg_info_list[pkg_name].add_md5sum(multi_name, f)
+                                                    pkg_version, pkg_arch,
+                                                    multi_var_name, dist)
+            else:
+                pkg_info_list[pkg_name].add_dist_info(pkg_version,
+                                                      multi_var_name, dist)
+
+            pkg_info_list[pkg_name].add_md5sum(multi_name, f, dist)
 
         print('Move {} to {}'.format(f, dst_path))
         if not args.dry_run:
@@ -245,13 +268,14 @@ def generate_makefile(args):
 
     for p in pkg_info_list.values():
         # Check for tag build packages
+        version = list(p.dist_info.values())[0]['version']
         if args.compare_dir is not None and compare_version_less(
-                p.version, (args.compare_dir / p.name).with_suffix('.mk')):
+                version, (args.compare_dir / p.name).with_suffix('.mk')):
             print('{}\'s version {} is less than latest. skip ...'.format(
-                p.name, p.version))
+                p.name, version))
             continue
         print('Generate {}\'s makefile with version {}'.format(
-            p.name, p.version))
+            p.name, version))
         with (makefile_dir / p.name).with_suffix('.mk').open('w') as f:
             p.generate_makefile(f, args.pkg_url_base)
 
